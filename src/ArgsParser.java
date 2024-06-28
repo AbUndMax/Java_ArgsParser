cae93a237aed54a938f77c0e12adaad589985555
Niklas <Max_Niklas@outlook.de>
2024-06-28 11:22:03 +0200

    Implement: Nested classes Parameter and ParameterList Refactor: parseArg(), getterMethods

--- src/ArgsParser.java
+++ src/ArgsParser.java
@@ -12,20 +12,17 @@ import java.util.*;
  * <li>after all parameters are added, the parseArgs method has to be called! (this is mandatory!)</li>
  * <li>then the arguments can be accessed with the getArgument methods</li>
  * </ol>
+ * <p> Made by Niklas Max G. 2024 </p>
  */
 public class ArgsParser {
 
     private String[] args;
-    private Set<String> argsSet;
-    private final Map<String, String> argumentsMap = new HashMap<>();
-    private final Map<String, String> shortFlagMap = new HashMap<>();
-    private final Set<String> allFlags = new HashSet<>();
-    private final Set<String> mandatoryFlags = new HashSet<>();
+    private final ParameterList argumentsList = new ParameterList();
+    private final Set<Parameter> mandatoryParameters = new HashSet<>();
     private boolean parsedSuccessfully = false;
 
     public ArgsParser(String[] args) {
         this.args = args;
-        this.argsSet = new HashSet<>(Arrays.asList(args));
     }
 
     /**
@@ -34,10 +31,9 @@ public class ArgsParser {
      * @param isMandatory true if parameter is mandatory, false if optional
      */
     public void addParameter(String flagName, boolean isMandatory) {
-        argumentsMap.put(flagName, null);
-        allFlags.add(flagName);
-
-        if(isMandatory) mandatoryFlags.add(flagName);
+        Parameter parameter = new Parameter(flagName);
+        argumentsList.addParameterToList(parameter);
+        if (isMandatory) mandatoryParameters.add(parameter);
     }
 
     /**
@@ -47,61 +43,44 @@ public class ArgsParser {
      * @param isMandatory true if parameter is mandatory, false if optional
      */
     public void addParameter(String flagName, String shortName, boolean isMandatory) {
-        addParameter(flagName, isMandatory);
-        shortFlagMap.put(shortName, flagName);
+        Parameter parameter = new Parameter(flagName, shortName);
+        argumentsList.addParameterToList(parameter);
+        if (isMandatory) mandatoryParameters.add(parameter);
     }
 
+    /**
+     * checks the given args Array and connects the argument of the user to the correct flag
+     */
     public void parseArgs() {
-        checkMandatoryParameters();
+
+        Set<Parameter> givenParameters = new HashSet<>();
+        Parameter currentParameter;
 
         for (int i = 0; i < args.length; i++) {
-            if (argumentsMap.containsKey(args[i])) {
-                try {
-                    argumentsMap.put(args[i], args[i + 1]);
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Error while parsing argument: " + args[i] + " " + args[i + 1]);
-                    System.exit(1);
-                }
+            if ((currentParameter = argumentsList.getParameterFromList(args[i])) != null) {
+                currentParameter.argument = args[i + 1];
+                givenParameters.add(currentParameter);
             }
+        }
+
+        if (!givenParameters.containsAll(mandatoryParameters)) {
+            mandatoryParameters.removeAll(givenParameters);
+            System.out.println("Mandatory parameters are missing: " + mandatoryParameters);
+            System.exit(1);
         }
 
         parsedSuccessfully = true;
     }
 
     /**
-     * checks if all mandatory parameters are set with an argument
-     * reports missing flags and exits program if not all mandatory parameters are set
-     */
-    private void checkMandatoryParameters() {
-        //find all fullName parameters in the given args:
-        Set<String> givenFlags = allFlags;
-        givenFlags.retainAll(argsSet);
-
-        //find all nicknames and convert them to fullNames and add them to the nameSet:
-        Set<String> givenShortFlags = shortFlagMap.keySet();
-        givenShortFlags.retainAll(argsSet);
-
-        for (String shortFlag : givenShortFlags) {
-            givenFlags.add(shortFlagMap.get(shortFlag));
-        }
-
-        //check if all mandatory parameters are set:
-        if (!givenFlags.containsAll(mandatoryFlags)) {
-            mandatoryFlags.removeAll(givenFlags);
-            System.out.println("Mandatory parameters are missing: " + mandatoryFlags);
-            System.exit(1);
-        }
-    }
-
-    /**
-     * returns the argument of the given flag
+     * returns the argument of the given flag (duplicate of getArgumentAsString)
      * @param flagName name of the flag
      * @return argument of the flag
      * @throws IllegalStateException if parseArgs() was not called before
+     * @throws IllegalArgumentException if a wrong or not existent flagName was given
      */
-    public String getArgument(String flagName) {
-        if (!parsedSuccessfully) throw new IllegalStateException("parseArgs() has to be called before getArgument()!");
-        return argumentsMap.get(flagName);
+    public String getArgument(String flagName) throws IllegalStateException, IllegalArgumentException {
+        return getArgumentAsString(flagName);
     }
 
     /**
@@ -109,10 +88,13 @@ public class ArgsParser {
      * @param flagName name of the flag
      * @return argument of the flag as a String
      * @throws IllegalStateException if parseArgs() was not called before
+     * @throws IllegalArgumentException if a wrong or not existent flagName was given
      */
-    public String getArgumentAsString(String flagName) {
-        if (!parsedSuccessfully) throw new IllegalStateException("parseArgs() has to be called before getArgumentAsString()!");
-        return argumentsMap.get(flagName);
+    public String getArgumentAsString(String flagName) throws IllegalStateException, IllegalArgumentException{
+        if (!parsedSuccessfully) throw new IllegalStateException("parseArgs() has to be called before getArgument()!");
+        Parameter param;
+        if ((param = argumentsList.getParameterFromList(flagName)) == null) throw new IllegalArgumentException();
+        return param.argument;
     }
 
     /**
@@ -120,10 +102,13 @@ public class ArgsParser {
      * @param flagName name of the flag
      * @return argument of the flag as an Integer
      * @throws IllegalStateException if parseArgs() was not called before
+     * @throws IllegalArgumentException if a wrong or not existent flagName was given
      */
-public int getArgumentAsInteger(String flagName) throws IllegalStateException {
+    public int getArgumentAsInteger(String flagName) throws IllegalStateException, IllegalArgumentException {
         if (!parsedSuccessfully) throw new IllegalStateException("parseArgs() has to be called before getArgumentAsInteger()!");
-        return Integer.parseInt(argumentsMap.get(flagName));
+        Parameter param;
+        if ((param = argumentsList.getParameterFromList(flagName)) == null) throw new IllegalArgumentException();
+        return Integer.parseInt(param.argument);
     }
 
     /**
@@ -131,10 +116,13 @@ public int getArgumentAsInteger(String flagName) throws IllegalStateException {
      * @param flagName name of the flag
      * @return argument of the flag as a Double
      * @throws IllegalStateException if parseArgs() was not called before
+     * @throws IllegalArgumentException if a wrong or not existent flagName was given
      */
-    public double getArgumentAsDouble(String flagName) throws IllegalStateException {
+    public double getArgumentAsDouble(String flagName) throws IllegalStateException, IllegalArgumentException {
         if (!parsedSuccessfully) throw new IllegalStateException("parseArgs() has to be called before getArgumentAsDouble()!");
-        return Double.parseDouble(argumentsMap.get(flagName));
+        Parameter param;
+        if ((param = argumentsList.getParameterFromList(flagName)) == null) throw new IllegalArgumentException();
+        return Double.parseDouble(param.argument);
     }
 
     /**
@@ -142,20 +130,86 @@ public int getArgumentAsInteger(String flagName) throws IllegalStateException {
      * @param flagName name of the flag
      * @return argument of the flag as a Boolean
      * @throws IllegalStateException if parseArgs() was not called before
+     * @throws IllegalArgumentException if a wrong or not existent flagName was given
      */
-    public boolean getArgumentAsBoolean(String flagName) throws IllegalStateException{
+    public boolean getArgumentAsBoolean(String flagName) throws IllegalStateException, IllegalArgumentException {
         if (!parsedSuccessfully) throw new IllegalStateException("parseArgs() has to be called before getArgumentAsBoolean()!");
-        return Boolean.parseBoolean(argumentsMap.get(flagName));
+        Parameter param;
+        if ((param = argumentsList.getParameterFromList(flagName)) == null) throw new IllegalArgumentException();
+        return Boolean.parseBoolean(param.argument);
     }
 
     /**
      * returns the argument of the given flag as a char
      * @param flagName name of the flag
-     * @return argument of the flag as a char - if input is longer than 1 character, only the first character is returned
+     * @return argument of the flag as a char - if input is longer than one character, only the first character is returned
      * @throws IllegalStateException if parseArgs() was not called before
+     * @throws IllegalArgumentException if a wrong or not existent flagName was given
      */
     public char getArgumentAsChar(String flagName) throws IllegalStateException{
         if (!parsedSuccessfully) throw new IllegalStateException("parseArgs() has to be called before getArgumentAsChar()!");
-        return argumentsMap.get(flagName).charAt(0);
+        Parameter param;
+        if ((param = argumentsList.getParameterFromList(flagName)) == null) throw new IllegalArgumentException();
+        return param.argument.charAt(0);
     }
+
+
+    /**
+     * class to keep the argument attributes together
+     */
+    private class Parameter {
+        private String flagName;
+        private String shortName;
+        private String argument;
+
+        public Parameter(String flagName) {
+            this.flagName = flagName;
+        }
+
+        public Parameter(String flagName, String shortName) {
+            this.flagName = flagName;
+            this.shortName = shortName;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Parameter parameter = (Parameter) o;
+            return Objects.equals(flagName, parameter.flagName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(flagName);
+        }
+
+    }
+
+
+    /**
+     * List to store the Parameters and a method to retrieve them
+     */
+    private class ParameterList extends LinkedList<Parameter> {
+
+        public void addParameterToList(Parameter parameter) {
+            add(parameter);
+        }
+
+        /**
+         * returns the parameter with the given flagName
+         * @param flag flagName (long or short version) of the parameter
+         * @return parameter with the given flagName
+         */
+        public Parameter getParameterFromList(String flag) {
+            for (Parameter p : this) {
+                if (p.flagName.equals(flag) || (p.shortName != null && p.shortName.equals(flag))) {
+                    return p;
+                }
+            }
+            return null;
+        }
+
+    }
+
 }
