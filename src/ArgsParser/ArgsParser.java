76bd9243079dff7e5f531506d74cc46961868db2
Niklas <Max_Niklas@outlook.de>
2024-07-10 12:41:21 +0200

    restructure code. generate HelpString inside CalledForHelpNotification class.

--- src/ArgsParser/ArgsParser.java
+++ src/ArgsParser/ArgsParser.java
@@ -65,7 +65,6 @@ public class ArgsParser {
     protected boolean parseArgsWasCalled = false;
     private int longestFlagSize = 0;
     private int longestShortFlag = 0;
-    private final int consoleWidth = 100;
 
     /**
      * Constructor demands the args array of the main method to be passed.
@@ -451,7 +450,7 @@ public class ArgsParser {
 
         if (oneArgProvided) {
             if (args[0].equals("--help") || args[0].equals("-h")) { // if --help or -h was called, the help is printed
-                throw new CalledForHelpNotification(generateHelpMessage(new HashSet<>(parameterMap.values())));
+                throw new CalledForHelpNotification(new HashSet<>(parameterMap.values()), longestFlagSize, longestShortFlag);
 
             } else if (firstArgumentIsParameter) { // if the first argument is a parameter but --help was not called, the program notifies the user of a missing argument
                 throw new MissingArgArgsException(args[0]);
@@ -462,7 +461,7 @@ public class ArgsParser {
 
         } else if (twoArgsProvided && (args[1].equals("--help") || args[1].equals("-h"))) {
             if (firstArgumentIsParameter) { // if the first argument is a parameter and --help follows,
-                throw new CalledForHelpNotification(generateHelpMessage(new HashSet<>(Collections.singletonList(parameterMap.get(args[0])))));
+                throw new CalledForHelpNotification(new HashSet<>(Collections.singletonList(parameterMap.get(args[0]))), longestFlagSize, longestShortFlag);
 
             } else { // if the first argument is not a parameter but --help was called,
                 // the program notifies the user of an unknown parameter input
@@ -531,160 +530,6 @@ public class ArgsParser {
             }
             throw new MandatoryArgNotProvidedArgsException(errorMessage.toString());
         }
-    }
-
-    /**
-     * prints all available Parameters found in argumentsList to the console
-     */
-    private String generateHelpMessage(Set<Parameter<?>> parameters) {
-        StringBuilder helpMessage = new StringBuilder();
-        helpMessage.append("\n");
-
-        String headTitle = " HELP ";
-        int spaceForHeadTitle = headTitle.length();
-        int numberOfHashes = consoleWidth / 2 - spaceForHeadTitle / 2;
-        String header = "#".repeat(numberOfHashes) + headTitle + "#".repeat(numberOfHashes);
-        helpMessage.append(header).append("\n");
-        helpMessage.append(centerString("[s]=String | [i]=Integer | [d]=Double | [b]=Boolean | [c]=Character")).append("\n");
-        helpMessage.append(centerString("(!)=mandatory parameter | (+)=optional parameter")).append("\n");
-        helpMessage.append("#").append("\n");
-
-        if (parameters.size() > 1) {
-            helpMessage.append(centerString("Available Parameters:")).append("\n");
-        }
-        helpMessage.append("#").append("\n");
-
-        for (Parameter<?> param : parameters) {
-            String helpString = parameterHelpString(param);
-            helpMessage.append(helpString).append("\n");
-            helpMessage.append("#").append("\n");
-        }
-
-        return helpMessage.append("#".repeat(consoleWidth)).toString();
-    }
-
-    /**
-     * centers a given string in the help Box
-     * @param stringToCenter String to be centered
-     * @return centered String with a leading #
-     */
-    private String centerString(String stringToCenter) {
-        int freeSpace = (consoleWidth - stringToCenter.length()) / 2 - 1;
-        return "#" + " ".repeat(freeSpace) + stringToCenter;
-    }
-
-    /**
-     * generates the help String (fullFlag, shortFlag, description) for a single Parameter
-     * @param parameter parameter Instance of which the help String should be generated
-     * @return String with all information for the given Parameter
-     */
-    private String parameterHelpString(Parameter<?> parameter) {
-        Map<String, String> shortFlagTypes = new HashMap<>(){{
-            put("String", "s");
-            put("Integer", "i");
-            put("Double", "d");
-            put("Boolean", "b");
-            put("Character", "c");
-        }};
-
-        String name = parameter.getFullFlag();
-        String shortFlag = parameter.getShortFlag() == null ? "/" : parameter.getShortFlag();
-        String description = parameter.getDescription() == null ? "No description available!" : parameter.getDescription();
-        String isMandatory = parameter.isMandatory() ? "(!)" : "(+)";
-        StringBuilder helpString = new StringBuilder("###  ");
-
-        // align the parameter names nicely
-        int nameWhiteSpaceSize = longestFlagSize - name.length();
-        name = name + " ".repeat(nameWhiteSpaceSize);
-        int shortWhiteSpaceSize = longestShortFlag == 0 ? 0 : longestShortFlag - shortFlag.length();
-        shortFlag = shortFlag + " ".repeat(shortWhiteSpaceSize);
-
-        // get type
-        String type = shortFlagTypes.get(parameter.getType());
-
-        helpString.append(name).append("  ").append(shortFlag).append("  [").append(type).append("] ").append(isMandatory).append("  ");
-        int whiteSpace = helpString.length();
-
-        // The description String gets checked if it fits inside the info box.
-        // If not, a new line will be added and the rest of the description will be aligned.
-        String[] descriptionRows = description.split(" \\n| \\n |\\n ", -1);
-        if (descriptionRows.length == 1) {
-            helpString.append(lineBreakAtWhitespace(description, whiteSpace));
-
-        } else {
-            for (int i = 0; i < descriptionRows.length; i++) {
-                String row = descriptionRows[i];
-                helpString.append(lineBreakAtWhitespace(row, whiteSpace));
-                if (i != descriptionRows.length - 1) helpString.append("\n#").append(" ".repeat(whiteSpace - 1));
-            }
-        }
-
-        // print default value if available
-        if (parameter.hasDefault()) {
-            String defaultTitle = "default: ";
-            String defaultValue = parameter.getDefaultValue().toString();
-            helpString.append("\n").append("#").append(" ".repeat(whiteSpace - defaultTitle.length() - 1)).append(defaultTitle);
-
-            if (whiteSpace + defaultValue.length() > consoleWidth) { // if the default is as large as the consoleWindow split default
-                helpString.append(lineBreak(defaultValue, whiteSpace));
-
-            } else {
-                helpString.append(defaultValue);
-            }
-        }
-
-        return helpString.toString();
-    }
-
-    /**
-     * helper function to do correct new lines if the Description is too long to fit into the help box
-     * @param description description that doesn't fit
-     * @param whiteSpace int that specifies how many white space before the description should be placed
-     * @return the concatenated lines
-     */
-    private String lineBreakAtWhitespace(String description, int whiteSpace) {
-        int restLength = description.length();
-        StringBuilder lineBuilder = new StringBuilder();
-
-
-        while (whiteSpace + restLength > consoleWidth) {
-            int i = consoleWidth - whiteSpace;
-            char currentChar = description.charAt(i);
-            while (currentChar != ' ') {
-                currentChar = description.charAt(--i);
-            }
-
-            lineBuilder.append(description, 0, i).append("\n#").append(" ".repeat(whiteSpace - 1));
-            description = description.substring(++i);
-            restLength = description.length();
-        }
-
-        return lineBuilder.append(description).toString();
-    }
-
-    /**
-     * helper function to do correct new lines if the defaultValue is too long to fit into the help box
-     * @param string defaultValue that doesn't fit
-     * @param whiteSpace int that specifies how many white space before the defaultValue should be placed
-     * @return the concatenated lines
-     */
-    private String lineBreak(String string, int whiteSpace) {
-        StringBuilder helpString = new StringBuilder();
-
-        int spaceForValue;
-        int staticFreeSpace = spaceForValue = consoleWidth - whiteSpace + 1;
-        String substring = string.substring(0, staticFreeSpace);
-        helpString.append(substring);
-
-        for (int i = 1; i < Math.ceil(string.length() / (double) staticFreeSpace); i++) { // print default line by line
-            if (spaceForValue + staticFreeSpace >= string.length())
-                substring = string.substring(spaceForValue);
-            else substring = string.substring(spaceForValue, spaceForValue += (staticFreeSpace - 1));
-
-            helpString.append("\n").append("#").append(" ".repeat(whiteSpace - 1)).append(substring);
-        }
-
-        return helpString.toString();
     }
 
 }
